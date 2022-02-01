90631d1d86735c2dd9950dadfb11e2cf09e555f6
Andrew Maney <andrewmaney05@gmail.com>
2022-02-01 11:20:38 -0500

    Add files via upload

---
+++ PythonKeyboardVirusThing/Compile_Vires.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Building...
+pyinstaller --exclude matplotlib --exclude scipy --exclude pandas --noconfirm --onefile --windowed .\KeyboardVires_lmao.py
+if %ERRORLEVEL% NEQ 0 goto ERR
+goto EOF
+
+
+:ERR
+echo ERROR!!!!!! >>>:((
+pause
+
+:EOF
+move .\dist\*.exe .\
+rmdir /s /q build
+rmdir /s /q dist
+rmdir /s /q __pycache__
+pause
+echo Done. Exitting in 5 seconds...
+timeout 5 > NUL
